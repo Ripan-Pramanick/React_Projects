c22ed62edfd679784044c86efbda2bf42d608f1b
Ripan-Pramanick <ripan_pramanick@hotmail.com>
2026-01-16 16:10:18 +0000

    hooks complete

---
+++ 01hooks/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  // let counter = 14;
+
+  const addValue = () => {
+    setCounter((counter += 1));
+
+    console.log(counter);
+  };
+
+  const removeValue = () => {
+    setCounter((counter -= 1));
+    console.log(counter);
+  };
+
+  return (
+    <>
+      <h1>Coffee With React</h1>
+      <h2>Counter Value : {counter}</h2>
+      
+      <button 
+      onClick={addValue}
+      >Add Value</button>
+
+      <button 
+      onClick={removeValue}
+      >Remove Value</button>
+    </>
+  );
+}
+
+export default App;
