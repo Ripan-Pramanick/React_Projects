4a5b21daa1a1887fe846ac73ee56d5e6be04f09d
Ripan-Pramanick <ripan_pramanick@hotmail.com>
2026-01-18 17:24:21 +0000

    bgChanger added

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="w-full h-screen duration-200">
+      Hello World
+     </div>
+    </>
+  )
+}
+
+export default App
